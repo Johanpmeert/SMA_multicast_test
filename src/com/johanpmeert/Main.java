884935b52d37d2164d74279b774adae99332b36c
johanpmeert <johan_meert@skynet.be>
2022-02-18 08:32:51 +0100

    Added watchdog to the SMA protocol. Multicast over wireless on a raspberry pi is not stable without it. Added external config.txt file

--- src/com/johanpmeert/Main.java
+++ src/com/johanpmeert/Main.java
@@ -3,12 +3,9 @@ package com.johanpmeert;
 import java.io.*;
 import java.net.*;
 
-import static javax.xml.bind.DatatypeConverter.parseHexBinary;
-import static javax.xml.bind.DatatypeConverter.printHexBinary;
-
 public class Main {
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
 
         final String sma_multicastIp = "239.12.255.254";
         final int sma_multicastPort = 9522;
@@ -21,16 +18,16 @@ public class Main {
             MulticastSocket mcSocket = new MulticastSocket(sma_multicastPort);
             mcSocket.joinGroup(group, netIf);
 
-            byte[] txbuf = parseHexBinary("534d4100000402a0ffffffff0000002000000000");  // discovery string to be sent to network, all SMA devices will answer
-            System.out.println("Sending out SMA specific discovery code " + printHexBinary(txbuf) + " to multicast address " + sma_multicastIp + "/" + sma_multicastPort);
+            byte[] txbuf = hexStringToByteArray("534d4100000402a0ffffffff0000002000000000"); // discovery string to be sent to network, all SMA devices will answer
+            System.out.println("Sending out SMA specific discovery code " + byteArrayToHexString(txbuf) + " to multicast address " + sma_multicastIp + "/" + sma_multicastPort);
             DatagramPacket data = new DatagramPacket(txbuf, txbuf.length, mcastAddr, sma_multicastPort);
             mcSocket.send(data);
 
-            byte[] buffer = new byte[10 * 1024];
+            byte[] buffer = new byte[1024];
             data = new DatagramPacket(buffer, buffer.length);
             while (true) {
                 mcSocket.receive(data);
-                String hexdata = printHexBinary(buffer);
+                String hexdata = byteArrayToHexString(buffer);
                 hexdata = hexdata.substring(0, data.getLength() * 2);
                 System.out.println("Received from " + data.getAddress() + ": " + hexdata);
             }
@@ -38,4 +35,27 @@ public class Main {
             System.out.println(e.toString());
         }
     }
+
+    public static byte[] hexStringToByteArray(String hex) {
+        hex = hex.length() % 2 != 0 ? "0" + hex : hex;
+        byte[] b = new byte[hex.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+            int index = i * 2;
+            int v = Integer.parseInt(hex.substring(index, index + 2), 16);
+            b[i] = (byte) v;
+        }
+        return b;
+    }
+
+    public static String byteArrayToHexString(byte[] bytes) {
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 }
